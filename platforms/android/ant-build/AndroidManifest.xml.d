e2b5dca827d1f3c04e3ff308335d14083c14c70e
melbeyt <melanieb@slalom.com>
2015-09-09 14:27:01 -0500

    Initial commit
    Initial commit of all files for working offline Incident Reporting
    prototype

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\ant-build\AndroidManifest.xml : \
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,6 @@
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\ant-build\CordovaApp.ap_ \
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\ant-build\classes.dex \
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\libs\gcm.jar \
+C:\cygwin64\home\melanieb\AppTests\PleaseWork\Please\platforms\android\libs\android-support-v13.jar \
